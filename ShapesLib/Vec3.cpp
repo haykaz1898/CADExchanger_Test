ebc00af97717d540f3ba1a7e60c4240fa8fbfaf0
haykaz1898 <32683398+haykaz1898@users.noreply.github.com>
2023-02-17 18:00:10 +0400

    Add files via upload
    -moved implementation of Vec3 to cpp file

--- ShapesLib/Vec3.cpp
+++ ShapesLib/Vec3.cpp
@@ -1,1 +1,36 @@
 #include "Vec3.h"
+
+std::string Vec3::to_string() const {
+	return "{ x = " + std::to_string(x) + "; y = " + std::to_string(y) + "; z = " + std::to_string(z) + " }";
+}
+
+Vec3& Vec3::operator=(const Vec3& other) {
+	if (this == &other)
+		return *this;
+	x = other.x;
+	y = other.y;
+	z = other.z;
+	return *this;
+}
+Vec3& Vec3::operator=(Vec3&& other) noexcept {
+	x = other.x;
+	y = other.y;
+	z = other.z;
+	return *this;
+}
+
+Vec3::Vec3() : x(0), y(0), z(0) { }
+Vec3::Vec3(const Vec3& other) {
+	*this = other;
+	if (this == &other)
+		return;
+	x = other.x;
+	y = other.y;
+	z = other.z;
+}
+Vec3::Vec3(Vec3&& other) noexcept {
+	x = other.x;
+	y = other.y;
+	z = other.z;
+}
+Vec3::Vec3(double x, double y, double z) : x(x), y(y), z(z) { }
--- ShapesLib/Vec3.h
+++ ShapesLib/Vec3.h
@@ -7,41 +7,14 @@ struct SHAPES_DLL_API Vec3 {
 	double y;
 	double z;
 	
-	std::string to_string() const {
-		return "{ x = " + std::to_string(x) + "; y = " + std::to_string(y) + "; z = " + std::to_string(z) + " }";
-	}
+	std::string to_string() const;
 
-	Vec3& operator=(const Vec3& other) {
-		if (this == &other)
-			return *this;
-
-		x = other.x;
-		y = other.y;
-		z = other.z;
-		return *this;
-	}
-	Vec3& operator=(Vec3&& other) noexcept {
-		x = other.x;
-		y = other.y;
-		z = other.z;
-		return *this;
-	}
+	Vec3& operator=(const Vec3& other);
+	Vec3& operator=(Vec3&& other) noexcept;
 	
-	Vec3() : x(0), y(0), z(0) { }
-	Vec3(const Vec3& other) {
-		*this = other;
-		if (this == &other)
-			return;
-
-		x = other.x;
-		y = other.y;
-		z = other.z;
-	}
-	Vec3(Vec3&& other) noexcept {
-		x = other.x;
-		y = other.y;
-		z = other.z;
-	}
-	Vec3(double x, double y, double z) : x(x), y(y), z(z) { }
+	Vec3();
+	Vec3(const Vec3& other);
+	Vec3(Vec3&& other) noexcept;
+	Vec3(double x, double y, double z);
 };
 
